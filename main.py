2f733b2d3e7640d2d5091d75738acecc838ad8ce
Juan Cruz <jc23729@gmail.com>
2020-12-03 22:23:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+
+
+import random
+
+random_coin_toss = random.randint(0,1)
+if random_coin_toss == 0:
+  print("heads")
+else:
+  print("tales")
+
+
+
+
+
+
+
+
+
